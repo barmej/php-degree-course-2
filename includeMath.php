533d1d0d6049f2f0af0623d6cb21af74a8b533e3
Menawer <careless.kw@gmail.com>
2019-09-17 22:17:13 +0300

    C2 L6 V2 R2

---
+++ includeMath.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once "math.php";
+include_once "math.php";
+include_once "math.php";
+
+echo square(4);
+
+?>
---
+++ math.php
@@ -0,0 +1,9 @@
+<?php
+
+function square($number){
+    return $number*$number;
+}
+
+echo "هذا الملف تم تضمينه";
+
+?>
